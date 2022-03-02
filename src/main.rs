309c2cecde05227355e47593e0d1dfe21ba77577
Robin Schroer <git@sulami.xyz>
2022-03-02 17:27:08 +0100

    Extract USB setup into its own function

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use bsp::entry;
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
 use hal::pac::{interrupt, CorePeripherals, Peripherals};
+use hal::time::KiloHertz;
 
 // USB logging
 use hal::usb::UsbBus;
@@ -28,8 +29,8 @@ use hal::sercom::v2::spi;
 use wifi_nina::transport;
 
 // Acellerometer
-use lsm6ds33;
 use lexical_core as lexical;
+use lsm6ds33::{AccelerometerOutput, GyroscopeOutput, Lsm6ds33};
 
 #[entry]
 fn main() -> ! {
@@ -43,45 +44,25 @@ fn main() -> ! {
     );
     let pins = bsp::Pins::new(peripherals.PORT);
     let mut led: bsp::Led = pins.led_sck.into();
-    let mut delay = Delay::new(core.SYST, &mut clocks);
 
-    let bus_allocator = unsafe {
-        USB_ALLOCATOR = Some(bsp::usb_allocator(
-            peripherals.USB,
+    unsafe {
+        setup_usb(
             &mut clocks,
+            peripherals.USB,
             &mut peripherals.PM,
             pins.usb_dm,
             pins.usb_dp,
-        ));
-        USB_ALLOCATOR.as_ref().unwrap()
-    };
-
-    unsafe {
-        USB_SERIAL = Some(SerialPort::new(bus_allocator));
-        USB_BUS = Some(
-            UsbDeviceBuilder::new(bus_allocator, UsbVidPid(0x2222, 0x3333))
-                .manufacturer("Fake company")
-                .product("Serial port")
-                .serial_number("TEST")
-                .device_class(USB_CLASS_CDC)
-                .build(),
+            &mut core,
         );
     }
-
-    unsafe {
-        core.NVIC.set_priority(interrupt::USB, 1);
-        NVIC::unmask(interrupt::USB);
-    }
+    let mut delay = Delay::new(core.SYST, &mut clocks);
 
     usb_log("usb initialised\n");
 
-    // Prep Nina
     let nina_cs = pins.nina_cs.into_push_pull_output();
     let nina_ack = pins.nina_ack.into_floating_input();
     let nina_rst = pins.nina_resetn.into_push_pull_output();
-    // let nina_gpio0 = pins.nina_gpio0.into_readable_output();
 
-    // Init SPI to Nina
     let gclk0 = clocks.gclk0();
     let clock = clocks.sercom2_core(&gclk0).unwrap();
     let miso = pins.nina_miso;
@@ -96,8 +77,6 @@ fn main() -> ! {
         .spi_mode(spi::MODE_0)
         .enable();
 
-    usb_log("SPI initialised\n");
-
     let transport =
         transport::SpiTransport::start(spi_master, nina_ack, nina_rst, nina_cs, |d: Duration| {
             delay.delay_us(d.as_micros() as u32)
@@ -106,15 +85,6 @@ fn main() -> ! {
     usb_log("transport initialised\n");
     let mut wifi = wifi_nina::Wifi::new(transport);
     usb_log("wifi initialised\n");
-    let fw_version = wifi.get_firmware_version();
-    usb_log("got wifi firmware version\n");
-    match fw_version {
-        Ok(version) => {
-            usb_log(str::from_utf8(version.as_slice()).unwrap());
-            usb_log("\n");
-        }
-        _ => usb_log("version bad\n"),
-    }
 
     usb_log("scanning networks...\n");
     match wifi.scan_networks() {
@@ -125,29 +95,31 @@ fn main() -> ! {
                     usb_log(ssid);
                     usb_log("\n");
                 }
-            };
-        },
+            }
+        }
         _ => usb_log("failed to scan\n"),
     }
 
     usb_log("Connecting to Gyro\n");
     let i2c = bsp::i2c_master(
         &mut clocks,
-        bsp::hal::time::KiloHertz(100),
+        KiloHertz(100),
         peripherals.SERCOM4,
         &mut peripherals.PM,
         pins.sda,
-        pins.scl);
-    let mut gyro = lsm6ds33::Lsm6ds33::new(i2c, 0x6A).unwrap();
-    gyro.set_gyroscope_output(lsm6ds33::GyroscopeOutput::Rate104).unwrap();
-    gyro.set_accelerometer_output(lsm6ds33::AccelerometerOutput::Rate104).unwrap();
+        pins.scl,
+    );
+    let mut gyro = Lsm6ds33::new(i2c, 0x6A).unwrap();
+    gyro.set_gyroscope_output(GyroscopeOutput::Rate104).unwrap();
+    gyro.set_accelerometer_output(AccelerometerOutput::Rate104)
+        .unwrap();
     usb_log("Connected to Gyro\n");
 
     loop {
         // delay.delay_ms(500u16);
-        // led.toggle().unwrap();
+        led.toggle().unwrap();
         // while ! gyro.gyro_data_available().unwrap() {}
-        while ! gyro.accel_data_available().unwrap() {}
+        while !gyro.accel_data_available().unwrap() {}
         // x-axis is along the length of the Arduino.
         // x-axis plus is away from the usb, y is on the power led side, z is down
         if let Ok((gx, gy, gz)) = gyro.read_accelerometer() {
@@ -195,7 +167,32 @@ fn poll_usb() {
     };
 }
 
+#[allow(non_snake_case)]
 #[interrupt]
 fn USB() {
     poll_usb();
+}
+
+/// Setup a USB client device.
+unsafe fn setup_usb(
+    clocks: &mut GenericClockController,
+    usb: bsp::pac::USB,
+    pm: &mut bsp::pac::PM,
+    usb_dm: impl Into<bsp::UsbDm>,
+    usb_dp: impl Into<bsp::UsbDp>,
+    core: &mut CorePeripherals,
+) {
+    USB_ALLOCATOR = Some(bsp::usb_allocator(usb, clocks, pm, usb_dm, usb_dp));
+    let bus_allocator = USB_ALLOCATOR.as_ref().unwrap();
+    USB_SERIAL = Some(SerialPort::new(bus_allocator));
+    USB_BUS = Some(
+        UsbDeviceBuilder::new(bus_allocator, UsbVidPid(0x2222, 0x3333))
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")
+            .device_class(USB_CLASS_CDC)
+            .build(),
+    );
+    core.NVIC.set_priority(interrupt::USB, 1);
+    NVIC::unmask(interrupt::USB);
 }
